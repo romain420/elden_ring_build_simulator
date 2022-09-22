c3cb5b6906ccc2c963fd9c27acf913fc76316ff2
Théo <tgueuret@live.fr>
2022-09-22 15:30:42 +0200

    updating schemas.py according to the work done previously on models.py

--- app/backend/app/schemas.py
+++ app/backend/app/schemas.py
@@ -9,14 +9,54 @@ from typing_extensions import Annotated
 #schema class User
 class User(BaseModel):
     id : str#Annotated[str, Field(default_factory=lambda: uuid4().hex)]
-    First_name : str
-    Last_name : str
-    date_of_birth : datetime
-    # start_date : datetime
-    email : str
+    First_name :        str
+    Last_name :         str
+    date_of_birth :     datetime
+    email :             str    
+    password:           str
+    created_at :        datetime
+    last_visit :        datetime
+    nb_builds :         int
+    builds :            list
 
     class Config:
         orm_mode = True
+
+
+#schema class User_build
+class User_build(BaseModel):
+    __tablename__ = "user_builds"
+
+    id :            str
+    name :          str
+    owner :         User
+    created_at :    datetime
+    last_visit :    datetime
+    last_update :   datetime
+    nb_visits :     int
+    items :         list
+    nb_items :      int
+
+    class Config:
+        orm_mode = True
+
+
+#schema class Item
+class Item(BaseModel):
+
+    id :            str
+    name :          str
+    image :         str
+    description :   str
+    category :      str
+    dmg_negation :  list
+    resistance :    list
+    weight :        int
+
+    class Config:
+        orm_mode = True
+
+#-------------------------------------------------------------------------#
 
 #schema class Build
 class Build(BaseModel):
