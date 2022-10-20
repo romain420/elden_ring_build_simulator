0467b43b3580dd06bc8a5e120928f39cc05fd0f6
RomainD_ <romain.Dreuilhet@protonmail.com>
2022-10-20 23:52:40 +0200

    Adding some responsive. it's working well for slider part but it's have to be review for the navbar part

--- app/frontend/react-docker/src/components/NavBar.js
+++ app/frontend/react-docker/src/components/NavBar.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiMenu, FiX } from 'react-icons/fi'
 import './NavBar.css'
 
-/*const navBarLinks = [
-    {url:"/home", title:"Home"}
-];*/
 
 export const NavBar = ({navBarLinks}) => {
+    const [menuClicked, setMenuClicked] = useState(false);
+    const toggleMenuClick = () => {
+        setMenuClicked(!menuClicked);
+    }
+
   return (
     <nav className='navbar'>
-        <span className='navbar-logo'>build</span>
-        <ul className='navbar-list'>
+        <span className='navbar-logo'>Elden Ring Build Creator</span>
+        {menuClicked ? (
+            <FiX size={25} className='navbar-menu' onClick={toggleMenuClick}/>
+        ) : (
+            <FiMenu size={25} className='navbar-menu' onClick={toggleMenuClick}/>
+        )}
+        <ul className={menuClicked ? "navbar-list" : "navbar-list navbar-list--active"}>
             {navBarLinks.map(item =>{
                 return (
                     <li className='navbar-item' key={item.title}>
