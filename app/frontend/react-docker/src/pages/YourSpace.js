5c956a4b19dbc4f41adcb3e74cea77f087731e62
RomainD_ <romain.Dreuilhet@protonmail.com>
2022-11-10 15:08:53 +0100

    creating the first call API to get info from elden ring API

--- app/frontend/react-docker/src/pages/YourSpace.js
+++ app/frontend/react-docker/src/pages/YourSpace.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { NavBar } from '../components/NavBar'
+import axios from "axios";
+import { NavBar } from '../components/NavBar';
 import { useParams } from 'react-router-dom';
+import { firstRequest } from '../services/eldenRing';
+import './YourSpace.css';
+
+
 //import '../components/NavBar.css'
 
 const navBarLinks = [
@@ -9,13 +14,32 @@ const navBarLinks = [
     {url:"/weapon", title:"Weapon"}
 ];
 
+const baseURL = "https://eldenring.fanapis.com/api/items?limit=2"
+
 export function YourSpace() {
     let { username } = useParams();
     const navBarTitle = "Your space";
+    let [itemDatas, setItemleDatas] = React.useState([]);
+
+    React.useEffect(() => {
+        firstRequest(baseURL).then(res => {
+            setItemleDatas(res.data);
+        })
+    }, []);
+    // React.useEffect(() => {
+    //     axios.get(baseURL).then((response) => {
+    //         setItemleDatas(response.data);
+    //     });
+    // }, []);
+
+    console.log("this itemdata :",itemDatas)
+
     return (
         <div>
             <NavBar navBarLinks={navBarLinks} navBarTitle={navBarTitle}/>
-            <div color='white'>bienveue dans ton espace {username}</div> 
+            <div className='welcome'>bienveue dans ton espace {username}</div> 
+            <div className='request'>{itemDatas[0]?.name}</div>
         </div>
+       
     )
 }
